abaf58fc52c306a1910736a0718ace2ef02f3d53
nyl_sauval <nyl_sauval@ens.univ_artois.fr>
2025-10-14 22:15:13 +0200

    resume cards for homepage created

---
+++ src/app/components/resume-card/resume-card.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+import {MatCard} from '@angular/material/card';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-resume-card',
+  imports: [
+    MatCard,
+    MatIcon,
+  ],
+  templateUrl: './resume-card.component.html',
+  styleUrl: './resume-card.component.scss'
+})
+export class ResumeCardComponent {
+  @Input() title!: string;
+  @Input() icon!: string;
+  @Input() value!: string | number;
+  @Input() color: string = 'bg-white';
+  @Input() subtitle!: string;
+}
